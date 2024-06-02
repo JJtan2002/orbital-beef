d56fa818f1c14a036da5729301061585b8c81902
Kitty-001 <jqm002@hotmail.com>
2024-06-02 13:17:53 +0800

    update settings.py to allow cors_origin

--- backend/cashflow/settings.py
+++ backend/cashflow/settings.py
@@ -139,6 +139,7 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 CORS_ALLOWED_ORIGINS = [
     "http://127.0.0.1:8000",
     "http://localhost:3000",
+    "https://orbital-beef.vercel.app/"
 ]
 
 CORS_ALLOW_CREDENTIALS = True
