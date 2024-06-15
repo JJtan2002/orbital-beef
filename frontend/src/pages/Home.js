6677889a71830a7364c22266af42ece95c339a58
JJtan2002 <tanjiajun2002@gmail.com>
2024-06-15 09:47:51 +0800

    Updated Home page to show dashboard button

--- frontend/src/pages/Home.js
+++ frontend/src/pages/Home.js
@@ -8,41 +8,40 @@ const Home = () => {
     let navigate = useNavigate();
 
     const { isLoggedIn, authTokens, name } = useAuth();
-
-    useEffect(() => {
-        if (isLoggedIn) {
-            navigate("/profile");
-            console.log("Jump to home");
-            console.log(authTokens.access);
-            console.log(jwtDecode(authTokens.access));
-        }
-    }, [isLoggedIn, navigate]);
-    
+    /*
+        useEffect(() => {
+            if (isLoggedIn) {
+                navigate("/profile");
+                console.log("Jump to home");
+                console.log(authTokens.access);
+                console.log(jwtDecode(authTokens.access));
+            }
+        }, [isLoggedIn, navigate]);
+      */
     return (
         <div>
             <h2 className="font-bold my-5 text-xl text-center">Welcome to CashFlow!</h2>
             <div>
                 <p className="my-5 text-l text-center w-4/5 mx-auto">Streamline your money experience with CashFlow!
-                    Here you can track your epenses, make your budgets, and generate financial reports.</p>
+                    Here you can track your expenses, make your budgets, and generate financial reports.</p>
             </div>
             <div className="text-center w-20px">
-                <a
-                    href="register"
-                    className="font-semibold leading-6 text-purple-600 hover:text-purple-500"
-                >
-                    Sign up and Explore!
-                </a>
+                {isLoggedIn ? (
+                    <a
+                        href="profile"
+                        className="font-semibold leading-6 text-purple-600 hover:text-purple-500"
+                    >
+                        Go to Dashboard
+                    </a>
+                ) : (
+                    <a
+                        href="register"
+                        className="font-semibold leading-6 text-purple-600 hover:text-purple-500"
+                    >
+                        Sign up and Explore!
+                    </a>
+                )}
             </div>
-            {isLoggedIn ? (
-                <a
-                    href="profile"
-                    className="font-semibold leading-6 text-purple-600 hover:text-purple-500"
-                >
-                    Go to Dashboard
-                </a>
-            ) : (
-                ""
-            )}
         </div>
     );
 };
