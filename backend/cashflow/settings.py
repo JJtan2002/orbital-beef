f51c866d87a32e04ca443bd33ef7595e7c3e1b0a
Kitty-001 <jqm002@hotmail.com>
2024-06-26 16:52:40 +0800

    bug

--- backend/cashflow/settings.py
+++ backend/cashflow/settings.py
@@ -193,8 +193,8 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 CORS_ALLOWED_ORIGINS = [
     # "http://localhost:8000",
     # "http://localhost:3000",
-    "https://orbital-beef.vercel.app/",
-    "https://orbital-beef-6446.vercel.app/"
+    "https://orbital-beef.vercel.app",
+    "https://orbital-beef-6446.vercel.app"
 ]
 
 CORS_ALLOW_HEADERS = [
