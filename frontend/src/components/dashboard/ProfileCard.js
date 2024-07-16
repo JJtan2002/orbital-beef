35860bc02c6146f45733a4647bb195f3da0c5c15
unknown <tanjiajun2002@gmail.com>
2024-07-16 10:13:23 +0800

    Fix slow loading of profile after login

--- frontend/src/components/dashboard/ProfileCard.js
+++ frontend/src/components/dashboard/ProfileCard.js
@@ -8,12 +8,12 @@ const ProfileCard = () => {
     const { isLoggedIn, user } = useAuth();
     const { profile, isPendingProfile } = usePro();
     
-    return isLoggedIn && !isPendingProfile && (
+    return isLoggedIn && !isPendingProfile && profile && (
         <div className="w-7/12 p-6 bg-white border border-gray-200 rounded-lg shadow dark:bg-gray-800 dark:border-gray-700">
             <div className="flex flex-col items-center pb-10">
                 <img alt="User Icon" width="96" height="96" src={UserIcon} className="mb-3 rounded-full shadow-lg" />
-                <h5 className="mb-1 text-xl font-medium text-gray-900 dark:text-white">{profile?.name}</h5>
-                <span className="text-sm text-gray-500 dark:text-gray-400">{profile?.email}</span>
+                <h5 className="mb-1 text-xl font-medium text-gray-900 dark:text-white">{profile.name}</h5>
+                <span className="text-sm text-gray-500 dark:text-gray-400">{profile.email}</span>
                 <div className="mt-4 flex space-x-3 lg:mt-6">
                     <a href="#"
                         className="inline-flex items-center rounded-lg bg-purple-700 px-4 py-2 text-center text-sm font-medium text-white hover:bg-purple-800 focus:outline-none focus:ring-4 focus:ring-purple-300 dark:bg-purple-600 dark:hover:bg-purple-700 dark:focus:ring-purple-800">
