82678e6f0b62b29aa6f16cb445c7b389181dc757
unknown <tanjiajun2002@gmail.com>
2024-07-23 17:20:40 +0800

    update tasks

--- backend/watchlist/tasks.py
+++ backend/watchlist/tasks.py
@@ -10,41 +10,40 @@ STOCKS = ['AAPL', 'MSFT', 'GOOGL', 'AMZN', 'TSLA', 'BRK.B', 'XOM','JPM',
 
 @shared_task
 def fetch_stock_data():
-    ticker = TEST_STOCK
-    response = requests.get(f'https://www.alphavantage.co/query?function=GLOBAL_QUOTE&symbol={ticker}&apikey={API_KEY}')
+    for ticker in STOCKS:
+        response = requests.get(f'https://www.alphavantage.co/query?function=GLOBAL_QUOTE&symbol={ticker}&apikey={API_KEY}')
     
-    if response.status_code == 200:
-        data = response.json().get("Global Quote")
-        if data:
-            open_price = data["02. open"]
-            low = data["04. low"]
-            high = data["03. high"]
-            close_price = data["05. price"]
-            date = data["07. latest trading day"]
-            change = data["09. change"]
-            change_percent = data["10. change percent"]
+        if response.status_code == 200:
+            data = response.json().get("Global Quote")
+            if data:
+                open_price = data["02. open"]
+                low = data["04. low"]
+                high = data["03. high"]
+                close_price = data["05. price"]
+                date = data["07. latest trading day"]
+                change = data["09. change"]
+                change_percent = data["10. change percent"]
 
-            # Print the data for testing purposes
-            print(f"Ticker: {ticker}, Date: {date}, Open: {open_price}, Low: {low}, High: {high}, Close: {close_price}, Change: {change}, Change Percent: {change_percent}")
+                # Print the data for testing purposes
+                print(f"Ticker: {ticker}, Date: {date}, Open: {open_price}, Low: {low}, High: {high}, Close: {close_price}, Change: {change}, Change Percent: {change_percent}")
 
-            # Save or update the stock data in the database
-            stock_data, created = StockData.objects.update_or_create(
-                id=f'{ticker}_{date}',
-                defaults={
-                    'ticker': ticker,
-                    'close_price': close_price,
-                    'date': date,
-                    'open_price': open_price,
-                    'low': low,
-                    'high': high,
-                    'change': change,
-                    'change_percent': change_percent,
-                },
-            )
+                # Save or update the stock data in the database
+                stock_data, created = StockData.objects.update_or_create(
+                    id=f'{ticker}_{date}',
+                    defaults={
+                        'ticker': ticker,
+                        'close_price': close_price,
+                        'date': date,
+                        'open_price': open_price,
+                        'low': low,
+                        'high': high,
+                        'change': change,
+                        'change_percent': change_percent,
+                    },
+                )
+            else:
+                print(f"No data found for ticker {ticker}")
         else:
-            print(f"No data found for ticker {ticker}")
-    else:
-        print(f"Failed to fetch data for ticker {ticker}, status code: {response.status_code}")
-
+            print(f"Failed to fetch data for ticker {ticker}, status code: {response.status_code}")
 # Call the function to test
 fetch_stock_data()
