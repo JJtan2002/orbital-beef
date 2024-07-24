3ca4de722dbf06bbce6cc2b14b47cbd693a15a43
Kitty-001 <jqm002@hotmail.com>
2024-07-25 04:39:00 +0800

    试试

--- backend/vercel-build.sh
+++ backend/vercel-build.sh
@@ -4,18 +4,6 @@
 echo "Install dependencies..."
 python3 -m pip install -r requirements.txt
 
-# Create the dist directory if it doesn't exist
-mkdir -p dist
-
-# # Add debug info
-# echo "Creating output files in dist..."
-
-# # Example: copying files to dist
-# cp -r your-source-directory/* dist/
-
-# # Check the contents of dist
-# ls -la dist
-
 # Apply migrations
 echo "Django managements..."
 python3 manage.py makemigrations
