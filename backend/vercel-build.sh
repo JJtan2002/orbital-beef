5682223514a4ef0343f5d00717d6e747cd9950e6
Kitty-001 <jqm002@hotmail.com>
2024-07-25 03:29:54 +0800

    try

--- backend/vercel-build.sh
+++ backend/vercel-build.sh
@@ -2,9 +2,9 @@
 
 # Install dependencies
 echo "Install dependencies..."
-pip install -r requirements.txt
+python3 -m pip install -r requirements.txt
 
 # Apply migrations
 echo "Django managements..."
-python manage.py makemigrations
-python manage.py migrate
+python3 manage.py makemigrations
+python3 manage.py migrate
